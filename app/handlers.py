85efa1760b005c373924c5fa0cb1616d6c8d5131
Shoban Chiddarth <shobanchiddarth.mail@gmail.com>
2026-08-07 14:10:55 +0530

    Created handler function for indexer proxy

---
+++ app/handlers.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import urllib3
+
+from fastapi import HTTPException
+
+
+
+
+
+
+def indexer_proxy(query: dict) -> dict:
+    """\
+    - payload must be the json DQL query for opensearch
+    """
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    url = f"{os.environ.get("WAZUH_INDEXER_HOST")}/wazuh-alerts-*/_search"
+
+    username = os.environ.get("WAZUH_USERNAME")
+    password = os.environ.get("WAZUH_PASSWORD")
+
+    response = requests.post(
+        url,
+        auth=(username, password),
+        headers = {
+            "Content-Type": "application/json"
+        },
+        json=query,
+        verify=False
+    )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+        return
+    
+    return response.json()
