9bcf5d26bdff4c3f80aa11801a4038f8b1f5cbdc
Shoban Chiddarth <shobanchiddarth.mail@gmail.com>
2026-08-07 14:14:57 +0530

    Added indexer proxy route

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
 
+from app.handlers import indexer_proxy
+
 router = APIRouter()
+
+
+router.add_api_route(
+    "/indexer-proxy",
+    indexer_proxy,
+    methods=["POST"],
+)
